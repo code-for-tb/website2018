01c71b744fd3b41f148a97f48c99b56c88a82649
Michael Bishop <miklb@miklb.com>
2018-06-07 04:25:07 -0400

    chore: add tests & Travis

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Website2018
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
